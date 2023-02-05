4753d6d4be5b52e6d027eb05c8cd9e7df0dd9635
RahulSG39 <rahulgirish39@gmail.com>
2023-02-05 20:24:21 +0530

    Week 3 assignment- closures, memoization, call, bind and apply

---
+++ Week-3/4.js
@@ -0,0 +1,17 @@
+function createStack() {
+  let items = [];
+  return {
+    push(item) {
+      items.push(item);
+    },
+    pop() {
+      return items.pop();
+    },
+  };
+}
+
+const stack = createStack();
+stack.push(10);
+stack.push(5);
+stack.pop();
+console.log(stack.items); // Undefined
